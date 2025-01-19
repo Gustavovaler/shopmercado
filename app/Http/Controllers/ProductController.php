f4f2b301243023f6ed6e33a2a01095a3bb2ee6f3
Gustavo Valerio <gustavovaler>
2025-01-19 01:48:29 -0300

    fix

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Models\Product;
 use MercadoPago\Client\Preference\PreferenceClient;
@@ -9,36 +10,43 @@ use MercadoPago\MercadoPagoConfig;
 
 class ProductController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $products =  Product::all();
         return view('products.index', compact('products'));
     }
 
-    public function checkout(Request $request){
+    public function checkout(Request $request)
+    {
         MercadoPagoConfig::setAccessToken('APP_USR-8013625678437650-011812-f50e9eb540bdb9b2f8e14d3ce4a614c9-1807710409');
         MercadoPagoConfig::setIntegratorId('24c65fb163bf11ea96500242ac130004');
         $sale = Product::find($request->input('product_id'));
 
         $client = new PreferenceClient();
         $preference = $client->create([
-        "excluded_payment_methods" => [["id" => "visa"]],
-        "installments" => 6,
-        "items"=> array(
-            array(
-            "title" => $sale->name,
-            "quantity" => 1,
-            "unit_price" => $sale->price,
-            "currency_id" => "ARS",
-            'description' => $sale->description
-            )
+            "payment_methods" => [
+                "excluded_payment_methods" => [["id" => "visa"]],
+                "installments" => 6,
+            ],
+            "items" => array(
+                array(
+                    "id" => $sale->id,
+                    "title" => $sale->name,
+                    "quantity" => 1,
+                    "unit_price" => $sale->price,
+                    "currency_id" => "ARS",
+                    'description' => $sale->description,
+                    'picture_url' => 'https://pick.com/photo/1234.jpg',
+                    "category_id" => 'baby'
+                )
             ),
-        "back_urls"=> [
-            "success" =>  "https://stockapp.store/back_urls/success",
-            "pending" =>  "https://stockapp.store/back_urls/pending",
-            "failure" =>  "https://stockapp.store/back_urls/failure"
-        ],
-        "external_reference" => "1643827245453",
-        
+            "back_urls" => [
+                "success" =>  "https://stockapp.store/back_urls/success",
+                "pending" =>  "https://stockapp.store/back_urls/pending",
+                "failure" =>  "https://stockapp.store/back_urls/failure"
+            ],
+            "external_reference" => "245453",
+
         ]);
         return view('products.checkout', compact('sale', 'preference'));
     }
